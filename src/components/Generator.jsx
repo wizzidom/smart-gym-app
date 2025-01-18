06c574c6ad4a04457230845f3693c7be99804922
wizzidom <majolawisdom@gmail.com>
2025-01-19 00:57:21 +0200

    Initial setup of project

---
+++ src/components/Generator.jsx
@@ -0,0 +1,7 @@
+export default function Generator() {
+  return (
+    <div>
+      <h1>Generator</h1>
+    </div>
+  );
+}
---
+++ src/components/Workout.jsx
@@ -0,0 +1,7 @@
+export default function Workout() {
+  return (
+    <div>
+      <h1>Workout</h1>
+    </div>
+  );
+}
